4c58a9466ad300b5d2980eda9f5a6fbe0110e514
LikynAI <Likyn2001@gmail.com>
2019-03-11 13:12:41 +0300

    Работа с потоками

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,18 +1,16 @@
 ﻿using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("MailSender")]
+[assembly: AssemblyTitle("treads2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MailSender")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("treads2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,25 +19,8 @@ using System.Windows;
 // COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
 
-//Чтобы начать создание локализуемых приложений, задайте
-//<UICulture>CultureYouAreCodingWith</UICulture> в файле .csproj
-//внутри <PropertyGroup>.  Например, если используется английский США
-//в своих исходных файлах установите <UICulture> в en-US.  Затем отмените преобразование в комментарий
-//атрибута NeutralResourceLanguage ниже.  Обновите "en-US" в
-//строка внизу для обеспечения соответствия настройки UICulture в файле проекта.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly: ThemeInfo(
-	ResourceDictionaryLocation.None, //где расположены словари ресурсов по конкретным тематикам
-									 //(используется, если ресурс не найден на странице,
-									 // или в словарях ресурсов приложения)
-	ResourceDictionaryLocation.SourceAssembly //где расположен словарь универсальных ресурсов
-											  //(используется, если ресурс не найден на странице,
-											  // в приложении или в каких-либо словарях ресурсов для конкретной темы)
-)]
-
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("bd3b7190-c9e5-409e-ae16-598b12dda13d")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
